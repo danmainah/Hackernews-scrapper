72a5497fd05cfe0ac49e200195dd2c000dc852aa
Daniel Maina <danielmainawaweru@gmail.com>
2025-01-18 12:54:44 +0300

    fix bugs to ensure fetching hosting credentials works correctly

--- src/db.js
+++ src/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 const fs = require('fs');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '12341234',
-    database: 'hacker_news',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 const initSql = fs.readFileSync('./src/init.sql', 'utf8');
